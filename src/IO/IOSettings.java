08d50a39c8b1cd2a78e5e6daa075215b5060f590
EmanueleC <emanuele.carraro.94@gmail.com>
2018-01-08 19:53:00 +0100

    fix path

--- src/IO/IOSettings.java
+++ src/IO/IOSettings.java
@@ -2,5 +2,5 @@ package IO;
 
 public class IOSettings {
     static final String DefaultInPath = "./terrier-core-4.2/var/results/";
-    static final String DefaultOutPath = "./../";
+    static final String DefaultOutPath = "./";
 }
