667a738b9927c50b7e324e559a6f336af98840bf
annabonaldo <annabonaldo92@gmail.com>
2018-01-10 16:19:46 +0100

    new data structures for Condorcet fusion alg

--- src/Fusion/Condorcet.java
+++ src/Fusion/Condorcet.java
@@ -1,77 +1,62 @@
 package Fusion;
+import Base.Run;
+import Base.RunLineScores;
+import Base.RunSet;
+import Fusion.CondorcetUtils.CondorcetGraph;
 
-import Base.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
-public class Condorcet
+class Condorcet
 {
-    
-    private RunSet _runSet;
-    
-    public Condorcet(RunSet runSet) {
-        _runSet = runSet;
-    }
-    
-    RunSet RunSet() { return _runSet; }
-    
-    public Run Fuse() {
-        Set<String> documentIDs = _runSet.getDocList();
-        String[] documentIDsArray = documentIDs.toArray(new String[documentIDs.size()]);
-        quickSort(documentIDsArray,0,documentIDs.size()-1);
+    public Run Fuse(RunSet runSet) {
+        Run run = new Run();
+        Set<String> queryAndTopicList = runSet.getQueryAndTopicList();
 
-        //TODO Stub return value
-        return new  Run();
+        for(String queryAndTopic :queryAndTopicList )
+        {
+            HashMap<String, ArrayList<RunLineScores>> filteredList =
+                    runSet.getDocsScoreListForQueryAndTopic(queryAndTopic);
+
+            CondorcetGraph graph = new CondorcetGraph(queryAndTopic, filteredList);
+
+
+        }
+
+
+        return run;
     }
-    
-    private void quickSort(String[] documentIDs, int left, int right) {
+
+
+    // quickSort(documentIDsArray,0,documentIDs.size()-1);
+ /*   private void quickSort(String[] documentIDs, int left, int right) {
         int index = partition(documentIDs, left, right);
-        
+
         if (left < index - 1) quickSort(documentIDs, left, index - 1);
         if (index < right) quickSort(documentIDs, index, right);
     }
-    
+
     private int partition(String[] documentIDs, int left, int right) {
         int i = left, j = right;
         String tmp;
         String pivot = documentIDs[(left + right) / 2];
-        
+
         while (i <= j) {
             while (comparison(documentIDs[i], pivot)) i++;
             while (comparison(documentIDs[j], pivot)) j--;
 
             if (i <= j) {
-                
-                  tmp = documentIDs[i];
-                  documentIDs[i] = documentIDs[j];
-                  documentIDs[j] = tmp;
-                  i++;
-                  j--;
-                  
-            }
-      }
-      return i;
-    }
-    
-    private boolean comparison(String doc1, String doc2) {
-        /*int count = 0;
-        Set<String> queryAndTopics = _runSet.getQueryAndTopicList();
-        Iterator<String> iterator = queryAndTopics.iterator();
-        while (iterator.hasNext()) {
-            String queryAndTopic = iterator.next();
-            String globalId1 = doc1 + queryAndTopic;
-            String globalId2 = doc2 + queryAndTopic;
-            ArrayList<RunLineScores> runLineScoresDoc1 = _runSet.getLineListAt(globalId1);
-            ArrayList<RunLineScores> runLineScoresDoc2 = _runSet.getLineListAt(globalId2);
-            for (RunLineScores runLineScore1 : runLineScoresDoc1) {
-                for (RunLineScores runLineScore2 : runLineScoresDoc2) {
-                    if (runLineScore1.Rank() > runLineScore2.Rank()) count ++;
-                    else count--;
-                }
+
+                tmp = documentIDs[i];
+                documentIDs[i] = documentIDs[j];
+                documentIDs[j] = tmp;
+                i++;
+                j--;
+
             }
         }
-        if (count > 0) return true;
-        else return false; */
-        return false;
+        return i;
     }
-    
+*/
 }
