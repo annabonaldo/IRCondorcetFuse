4b692c2d6ee57a015624c84881b19d1b634ba9fd
annabonaldo <annabonaldo92@gmail.com>
2018-01-26 09:28:45 +0100

    Settings changes

--- src/IO/Settings.java
+++ src/IO/Settings.java
@@ -19,14 +19,22 @@ public class Settings {
     public static final Boolean FuseSUM =true;
     public static final Boolean FuseMIN =true;
     public static final Boolean FuseANZ =true;
+    public static final Boolean FuseANZ =true;
     public static final Boolean FuseMED =true;
-    public static final Boolean FuseCONDORCET =true;
+    public static final Boolean FuseCONDORCET =false;
 
 
     public static final Boolean ALLFusionInDir = false;
-    public static final String[] SUBSET_runBasic = {"runBasic_2/", "runBasic_4/","runBasic_6/",
-                                                    "runBasic_8/", "runBasic_10/"};
-    public static final String[] SUBSET_FusionInDir = SUBSET_runBasic;
+    public static final String[] SUBSET_runBasic_K100 =
+            {"runBasic_k100_2/", "runBasic_k100_4/","runBasic_k100_6/", "runBasic_k100_8/", "runBasic_k100_10/"};
+
+    public static final String[] SUBSET_runBasic =
+            {"runBasic_2/", "runBasic_4/","runBasic_6/", "runBasic_8/", "runBasic_10/"};
+
+    public static final String[] SUBSET_runQE =
+            {"runQE_2/", "runQE_4/","runQE_6/", "runQE_8/", "runQE_10/"};
+
+    public static final String[] SUBSET_FusionInDir = SUBSET_runQE; //{"runBasic_10/"};
 
 
 }
